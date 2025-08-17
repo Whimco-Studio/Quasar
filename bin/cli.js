060e7ce62612bb264e486f65a62653d08721ca34
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2025-08-17 18:29:42 -0400

    Initial project setup with essential files: added package.json, package-lock.json, .gitignore, and CLI implementation. Implemented core functionalities for app creation, building, and watching for changes.

---
+++ bin/cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+"use strict";
+const { join } = require("path");
+
+function cmd(name) {
+  return require(join(__dirname, "..", "lib", name + ".js"));
+}
+const [, , sub = "help", ...rest] = process.argv;
+
+const map = {
+  init: () => require("../lib/init.js").run?.() ?? require("../lib/init.js").initQuasar?.(),
+  "create-app": () => require("../lib/createApp.js").run?.(rest[0]) ?? require("../lib/createApp.js").createApp?.(rest[0]),
+  "generate-project": () => require("../lib/generateProject.js").run?.() ?? require("../lib/generateProject.js").generateProjectJson?.(),
+  watch: () => require("../lib/watch.js").run(),
+  build: () => require("../lib/build.js").run(),
+  help: () => console.log(`quasar <command>
+  init                 scaffold config/tsconfig hooks
+  create-app <name>    scaffold self-contained app
+  generate-project     write default.project.json
+  watch                chokidar watch -> codegen -> rbxtsc
+  build                one-shot codegen -> rbxtsc`)
+};
+
+if (!map[sub]) map.help(); else map[sub]();
+
